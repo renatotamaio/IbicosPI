633ef07518126b99874848129f77a09eaae752e0
Matheus Eduardo França <matheusefranca1727@gmail.com>
2022-09-24 00:44:39 -0300

    Component de Snackbar global

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,16 +34,21 @@
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
+
+    <SnackbarComponent />
   </v-app>
 </template>
 
 <script>
+import SnackbarComponent from "./components/SnackbarComponent.vue";
 
 export default {
-  name: 'App',
-
+  name: "App",
+  components: {
+    SnackbarComponent,
+  },
   data: () => ({
     //
   }),
---
+++ src/components/SnackbarComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-snackbar
+    top
+    right
+    multi-line
+    :color="snackbar.color"
+    v-model="snackbar.show"
+  >
+    <slot name="content">
+      {{ snackbar.text }}
+    </slot>
+
+    <template v-slot:action="{ attrs }">
+      <slot name="button">
+        <v-btn
+          :color="snackbar.buttonColor"
+          text
+          v-bind="attrs"
+          @click="snackbar.show = false"
+        >
+          Fechar
+        </v-btn>
+      </slot>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "SnackbarComponent",
+  computed: {
+    snackbar: {
+      get() {
+        return this.$store.getters["snackbar/getSnackbar"];
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
