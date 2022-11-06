521d37f96c1977a3ba9eda5c2b2e6b6508e4c3c6
MatheusFr27 <matheusefranca1727@gmail.com>
2022-11-06 02:34:40 +0000

    Implementando Layout normal

--- src/store/stores/config/state.js
+++ src/store/stores/config/state.js
@@ -1,5 +1,6 @@
 const DefaultLayout = () => import("../../../layouts/DefaultLayout.vue");
 const AuthLayout = () => import("../../../layouts/AuthLayout.vue");
+const NormalLayout = () => import('../../../layouts/NormalLayout.vue');
 
 export default {
   layouts: {
@@ -9,5 +10,8 @@ export default {
     auth: {
       component: AuthLayout,
     },
+    normal: {
+      component: NormalLayout
+    }
   },
 };
